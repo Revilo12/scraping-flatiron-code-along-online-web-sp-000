f8c5013ea61a6b65bc499628d4d6e29fcf894298
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-13 20:12:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
-  
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,8 +16,30 @@ class Scraper
       end
     end
   end
-  
+
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry
+  end
+
+  # def get_page(link)
+  #   page = open(link)
+  #   Nokogiri::HTML(page).doc
+  # end
+  #
+  # def get_courses(html)
+  #   doc = html.css(.class1.class2)
+  #
+  # end
+  #
+  # def make_courses(course_doc)
+  #   course_doc.each do |course_html|
+  #     course = Course.new
+  #     course.title = course_html
+  #     course.schedule = course_html
+  #     course.description = course_html
+  #   end
+  # end
 end
 
-
-
+Scraper.new.get_page
